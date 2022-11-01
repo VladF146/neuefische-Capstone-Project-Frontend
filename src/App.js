6612bdcb9491cfb08f716e80b33e8e6272424c4b
VladF146 <vlad.fast146@gmail.com>
2022-11-01 10:34:34 +0100

    install react-router and configure basic routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path="/" element={"Home"} />
+        <Route path="/signup" element={"Signup"} />
+        <Route path="/signin" element={"Signin"} />
+        <Route path="*" element={"No page found"} />
+      </Routes>
     </div>
   );
 }
