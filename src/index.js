2783473d9ddc115038a32c6a8f68eb612eab8af0
VladF146 <vlad.fast146@gmail.com>
2022-11-01 11:46:48 +0100

    created an authentication context, to check for signup status

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import AuthenticationContextProvider from "./Contexts/AuthenticationContenxt";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthenticationContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthenticationContextProvider>
   </React.StrictMode>
 );
