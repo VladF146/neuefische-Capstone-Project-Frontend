9408aec7b42ac3f26c28d28150930032c4d97bd5
VladF146 <vlad.fast146@gmail.com>
2022-11-07 17:13:13 +0100

    REFACTORING: renamed navbar link from edit to create and added matching plus icon

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -35,10 +35,10 @@ function Navbar() {
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+                d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
               />
             </svg>
-            Edit
+            Create
           </StyleNavLink>
         </li>
         <li>
