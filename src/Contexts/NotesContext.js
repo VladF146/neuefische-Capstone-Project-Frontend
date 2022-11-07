1f25722d485548af11bf943ac8c72ddd9fbd2804
VladF146 <vlad.fast146@gmail.com>
2022-11-07 13:56:57 +0100

    REFACTORING: corrected a small naming error

--- src/Contexts/NotesContext.js
+++ src/Contexts/NotesContext.js
@@ -2,7 +2,7 @@ import { createContext, useReducer } from "react";
 
 export const NotesContext = createContext();
 
-export const noteActionTypes = {
+export const notesActionTypes = {
   GET_ALL_NOTES: "GET_ALL_NOTES",
   POST_SINGLE_NOTE: "POST_SINGLE_NOTE",
   UPDATE_SINGLE_NOTE: "UPDATE_SINGLE_NOTE",
@@ -11,21 +11,21 @@ export const noteActionTypes = {
 
 const notesReducer = (state, action) => {
   switch (action.type) {
-    case noteActionTypes.GET_ALL_NOTES:
+    case notesActionTypes.GET_ALL_NOTES:
       return {
         notes: action.payload,
       };
-    case noteActionTypes.POST_SINGLE_NOTE:
+    case notesActionTypes.POST_SINGLE_NOTE:
       return {
         notes: [action.payload, ...state.notes],
       };
-    case noteActionTypes.UPDATE_SINGLE_NOTE:
+    case notesActionTypes.UPDATE_SINGLE_NOTE:
       return {
         notes: state.notes.map((element) =>
           element._id === action.payload._id ? action.payload : element
         ),
       };
-    case noteActionTypes.DELETE_SINGLE_NOTE:
+    case notesActionTypes.DELETE_SINGLE_NOTE:
       return {
         notes: state.notes.filter(
           (element) => element._id !== action.payload._id
