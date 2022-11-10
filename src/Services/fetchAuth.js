5af97cb86f741718fe35155791c0edf0bcbe98b9
VladF146 <vlad.fast146@gmail.com>
2022-11-10 13:16:26 +0100

    replaced fetch with axios

--- src/Services/fetchAuth.js
+++ src/Services/fetchAuth.js
@@ -1,23 +1,11 @@
+import axios from "axios";
+
 const API_BASE_URL =
   process.env.API_BASE_URL ||
   "https://neuefische-capstone-backend.herokuapp.com";
 
-export const postSignin = async (email, password) => {
-  const response = await fetch(`${API_BASE_URL}/api/users/signin`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ email, password }),
-  });
-  const data = await response.json();
-  return { response, data };
-};
+export const postSignin = (email, password) =>
+  axios.post(`${API_BASE_URL}/api/users/signin`, { email, password });
 
-export const postSignup = async (email, password) => {
-  const response = await fetch(`${API_BASE_URL}/api/users/signup`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ email, password }),
-  });
-  const data = await response.json();
-  return { response, data };
-};
+export const postSignup = (email, password) =>
+  axios.post(`${API_BASE_URL}/api/users/signup`, { email, password });
--- src/Services/fetchNotes.js
+++ src/Services/fetchNotes.js
@@ -1,56 +1,48 @@
+import axios from "axios";
+
 const API_BASE_URL =
   process.env.API_BASE_URL ||
   "https://neuefische-capstone-backend.herokuapp.com";
 
-export const getAllNotes = async (user) => {
-  const response = await fetch(`${API_BASE_URL}/api/notes`, {
-    headers: { Authorization: `Bearer ${user?.token}` },
-  });
-  const data = await response.json();
-  return { response, data };
-};
-
-export const getSingleNote = async (user, noteId) => {
-  const response = await fetch(`${API_BASE_URL}/api/notes/${noteId}`, {
-    headers: { Authorization: `Bearer ${user?.token}` },
-  });
-  const data = await response.json();
-  return { response, data };
-};
-
-export const createSingleNote = async (user, title, content) => {
-  const response = await fetch(`${API_BASE_URL}/api/notes`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${user?.token}`,
-    },
-    body: JSON.stringify({ title, content }),
-  });
-  const data = await response.json();
-  return { response, data };
-};
-
-export const updateSingleNote = async (user, noteId, title, content) => {
-  const response = await fetch(`${API_BASE_URL}/api/notes/${noteId}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${user?.token}`,
-    },
-    body: JSON.stringify({ title, content }),
-  });
-  const data = await response.json();
-  return { response, data };
-};
-
-export const deleteSingleNote = async (user, noteId) => {
-  const response = await fetch(`${API_BASE_URL}/api/notes/${noteId}`, {
-    method: "DELETE",
+export const getAllNotes = (user) =>
+  axios.get(`${API_BASE_URL}/api/notes`, {
     headers: {
       Authorization: `Bearer ${user?.token}`,
     },
   });
-  const data = await response.json();
-  return { response, data };
-};
+
+export const getSingleNote = (user, noteId) =>
+  axios.get(`${API_BASE_URL}/api/notes/${noteId}`, {
+    headers: {
+      Authorization: `Bearer ${user?.token}`,
+    },
+  });
+
+export const createSingleNote = (user, title, content) =>
+  axios.post(
+    `${API_BASE_URL}/api/notes`,
+    { title, content },
+    {
+      headers: {
+        Authorization: `Bearer ${user?.token}`,
+      },
+    }
+  );
+
+export const updateSingleNote = (user, noteId, title, content) =>
+  axios.put(
+    `${API_BASE_URL}/api/notes/${noteId}`,
+    { title, content },
+    {
+      headers: {
+        Authorization: `Bearer ${user?.token}`,
+      },
+    }
+  );
+
+export const deleteSingleNote = (user, noteId) =>
+  axios.delete(`${API_BASE_URL}/api/notes/${noteId}`, {
+    headers: {
+      Authorization: `Bearer ${user?.token}`,
+    },
+  });
