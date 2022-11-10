69ec7b2939c8dbb06c899e5c8df22e16fd57098c
VladF146 <vlad.fast146@gmail.com>
2022-11-10 22:18:21 +0100

    improved toggle styling

--- src/Components/Toggle.styles.js
+++ src/Components/Toggle.styles.js
@@ -5,14 +5,16 @@ const Styled = {};
 Styled.ToggleContainer = styled.div`
   display: flex;
   padding: 0.125rem;
-  margin-left: 0.25rem;
   border-radius: 0.5rem;
   background-color: #f8fafc;
+  align-self: end;
+  box-shadow: inset 0px 0px 10px 0 rgba(0, 0, 0, 0.1);
+
 `;
 Styled.ToggleButton = styled.button`
   display: flex;
-  padding: 0.5rem;
-  background-color: ${({ activated }) => (activated ? '#ffffff' : '#f8fafc')};
+  padding: 0.875rem;
+  background-color: ${({ activated }) => (activated ? '#ffffff' : 'transparent')};
   font-size: 0.875rem;
   line-height: 1.25rem;
   font-weight: 600;
@@ -22,6 +24,9 @@ Styled.ToggleButton = styled.button`
     ? '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)'
     : '')};
   border: unset;
+  color: ${({ activated }) => (activated
+    ? 'black'
+    : '#9ca3af')};
   cursor: pointer;
 
   svg {
